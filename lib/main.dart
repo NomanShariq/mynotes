98d7992f3fcd49b3cb0bc14babfb17eb39b80c2b
Nomanshariq562 <86268414+Nomanshariq562@users.noreply.github.com>
2023-01-16 20:53:41 +0500

    checking push

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/firebase_options.dart';
-import 'package:mynotes/views/login_view.dart';
-import 'package:mynotes/views/register_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,7 +24,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Homepage'),
+          title: const Text('Homepage'),
         ),
         body: FutureBuilder(
             future: Firebase.initializeApp(
